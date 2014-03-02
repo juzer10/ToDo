4d09db33bc118291e668e2b6d767fc272a742e76
Juzer Khambaty <juzerkhambaty@gmail.com>
2014-03-02 21:21:44 +0530

    Initial Sync

---
+++ ToDo/src/main/java/todo/list/SplashScreen.java
@@ -0,0 +1,32 @@
+package todo.list;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+/**
+ * Created by Juzer on 2/28/14.
+ */
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final int sec = 1000;
+        setContentView(R.layout.splash_screen);
+        new Thread(new Runnable() {
+            public void run()
+            {
+                try {
+                    Thread.sleep(sec);
+                    Intent i = new Intent(SplashScreen.this, ToDoList.class);
+                    startActivity(i);
+                    finish();
+                }
+                catch(Exception e) {
+            }
+            }
+        }).start();
+    }
+}
